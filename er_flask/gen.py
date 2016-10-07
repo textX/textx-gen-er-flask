9484b363e83c98391c9fe0d37203ce26f5807de5
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-10-07 17:31:20 +0200

    WIP Mapping from ER to SQLAlchemy. flask SQLAlchemy template.

--- er_flask/gen.py
+++ er_flask/gen.py
@@ -1,10 +1,12 @@
 import os
+from collections import namedtuple
 from jinja2 import Environment, PackageLoader, FileSystemLoader, ChoiceLoader
 from txtools.gen import GenDesc
 from txtools.exceptions import TextXToolsException
 from textx.lang import get_language
-from textx.model import all_of_type
-from er.lang import get_constraint
+from textx.model import children_of_type, model_root, parent_of_type
+from er.lang import get_constraint, is_entity_ref, is_enum_ref,\
+    attr_type, meta_name
 
 
 PARAM_NAMES = ('flask_admin', 'composite_keys')
@@ -50,6 +52,178 @@ def all(obj, type_name):
     return all_of_type(lang, obj, type_name)
 
 
+# Structure used to capture relational meta-data
+Column = namedtuple('Column', 'name dbname pk fk fk_target dbtype nullable')
+Relationship = namedtuple('Relationship', 'name target_class fk_columns')
+ForeignKeyConstraint = namedtuple('ForeignKeyConstraint',
+                                  'from_col_names target_table to_col_names')
+
+
+def ent_elements(ent):
+    """
+    For the given entity returns all columns and relationships.
+    Columns are deduced from ER attributes and * references to this entity
+    following rules outlined in Notes.org.
+    """
+
+    elements = []
+
+    # Find all referers and add columns and relationships.
+    for e in children_of_type(model_root(ent), "Entity"):
+        for attr in children_of_type(e, "Attribute"):
+            if attr.type.type is ent and attr.multiplicity.upper == '*':
+                elements.extend(columns_target(attr))
+
+    # Add columns and relationships from the direct attributes
+    for attr in children_of_type(ent, "Attribute"):
+        # 'Many' attributes will have columns in target entity table.
+        if attr.multiplicity.upper != '*':
+            elements.extend(columns(attr))
+        # elements.extend(rel(attr))
+
+    return elements
+
+
+def columns(attr):
+    """
+    Returns columns introduced by the given ER attribute.
+    """
+
+    if is_entity_ref(attr):
+        tattrs = pk_attrs(attr_type(attr))
+        fk = len(tattrs) == 1
+    else:
+        tattrs = [attr]
+        fk = False
+    pk = attr.id
+
+    # In case of multiple references to the same Entity we need unique
+    # column names. Using counter. TODO.
+    # counter = 1
+    # referenced_entity_counter = {}
+    # if clsname == 'Entity':
+    #     if ent.name not in referenced_entity_counter:
+    #         referenced_entity_counter[ent.name] = 1
+    #     else:
+    #         referenced_entity_counter[ent.name] += 1
+    #         counter = referenced_entity_counter[ent.name]
+
+    # Columns
+    columns = []
+    for a in tattrs:
+        fk_target = ''
+        if fk:
+            fk_target = '{}.{}'.format(dbname(attr_type(attr)), dbname(a))
+            attr_name = attr.name
+            col_name = dbname(attr)
+        else:
+            attr_name = a.name
+            col_name = dbname(a)
+
+        nullable = attr.multiplicity.lower == 0
+        columns.append(
+            Column(name=attr_name,
+                   dbname=col_name,
+                   pk=pk,
+                   fk=fk,
+                   fk_target=fk_target,
+                   dbtype=dbtype(a),
+                   nullable=nullable))
+
+    return columns
+
+
+def columns_target(attr):
+    """
+    Returns columns introduced by the other side attribute referencing this
+    attr entity using many reference (*).
+    """
+    tattrs = pk_attrs(parent_of_type(attr, "Entity"))
+    pk = True
+
+    # column will have FK constraint if it is single column
+    fk = len(tattrs) == 1
+
+    # Columns
+    columns = []
+    for a in tattrs:
+        fk_target = ''
+        if fk:
+            fk_target = '{}.{}'.format(attr_type(attr), dbname(a))
+        if attr.multiplicity.lower == 0:
+            nullable = True
+        if len(tattrs) == 1:
+            attr_name = attr.name
+            col_name = dbname(attr)
+        else:
+            attr_name = a.name
+            col_name = dbname(a)
+
+            columns.append(
+                Column(name=attr_name,
+                       dbname=col_name,
+                       pk=pk,
+                       fk=fk,
+                       fk_target=fk_target,
+                       dbtype=dbtype(a),
+                       nullable=nullable))
+
+    return columns
+
+
+def rel(attr):
+    """
+    Returns relationship for the given ER attribute.
+    """
+
+
+def rel_target(attr):
+    """
+    Returns relationship for the other side attribute referencing this attr
+    entity.
+    """
+
+
+def pk_attrs(ent):
+    """
+    Return a list of attributes reachable by recursively following id attributes
+    in target entities.
+    """
+    attrs = []
+    for a in ent.attributes:
+        if a.id:
+            if not is_entity_ref(a):
+                attrs.append(a)
+            else:
+                attrs.extend(pk_attrs(a.type.type))
+    return attrs
+
+
+def dbtype(attr):
+    """
+    Returns SQLAlchemy type for the given er attribute.
+    """
+    assert not is_entity_ref(attr)
+    tname = attr_type(attr).name
+    if is_enum_ref(attr):
+        return 'db.Enum({})'.format(attr_type(attr).name)
+    else:
+        if tname in ['int', 'time', 'date']:
+            # Simple types
+            return {
+                'int': 'db.Integer',
+                'time': 'db.DateTime',
+                'date': 'db.Date',
+            }[tname]
+        elif tname in ['string']:
+            # Types with precision
+            return "{}({})".format({
+                'string': 'db.String',
+            }[tname], attr.type.precision_x)
+        elif tname == 'decimal':
+            return 'db.Float({}, asdecimal=True)'.format(attr.type.precision_x)
+
+
 def render(template_path, context, root_path=None):
     """
     Returns rendered template. By default search for template at the given
@@ -69,7 +243,7 @@ def render(template_path, context, root_path=None):
     ]))
 
     env.filters['dbname'] = dbname
-    env.filters['all'] = all
+    env.filters['ent_elements'] = ent_elements
 
     return env.get_template(template_path).render(**context)
 
--- er_flask/templates/flask.py
+++ er_flask/templates/flask.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
-from flask_admin.contrib.sqla import ModelView
-from flask_admin import Admin
+# from flask_admin.contrib.sqla import ModelView
+# from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+import enum
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -21,17 +23,27 @@ def initdb():
 def index():
     return '<a href="admin/">Click me to go to admin!</a>'
 
+{% for enum in enums %}
+class {{enum.name}}(enum.Enum):
+    {%- for lit in enum.literals %}
+    {{lit.name}} = "{{lit.code}}"
+    {%- endfor %}
+
+{% endfor %}
 
 {% for entity in entities %}
 class {{entity.name}}(db.Model):
     __tablename__ = '{{entity|dbname}}'
 
-    {% for attr in entity|all("Attribute") %}
-    {{attr.name}} = db.Column('{{attr|dbname}}')
+    {% for elem in entity|ent_elements %}
+    {%- if elem.__class__.__name__ == 'Column' %}
+    {{elem.name}} = db.Column('{{elem.dbname}}', {{elem.dbtype}}
+                              {%- if elem.fk %}, db.ForeignKey('{{elem.fk_target}}'){% endif %}
+                              {%- if elem.pk %}, primary_key=True{% endif %}
+                              {%- if elem.nullable %}, nullable=True{% endif %})
+    {%- else %}
+    {%- endif %}
     {%- endfor %}
 
 {% endfor %}
 
-{% for enum in enums %}
-Enum: {{enum.name}}
-{% endfor %}
